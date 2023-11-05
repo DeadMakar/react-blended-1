41dd22a95c3fc347c0de152668e047633f74313b
DeadMakar <ssapientiaa@gmail.com>
2023-11-05 11:07:00 +0200

    task1

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,48 @@
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+import { formatDateToNow } from 'helpers/formatDate';
+import {
+  Avatar,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardPoster,
+  CardText,
+  CardTitle,
+  Date,
+  Tag,
+  UserBox,
+  UserInfo,
+  UserName,
+} from './BlogCard.styled';
+
+export const BlogCard = ({
+  name,
+  postedAt,
+  poster,
+  tag,
+  title,
+  description,
+  avatar,
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster src={poster} alt={tag} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={name} />
+          <UserInfo>
+            <UserName>{name}</UserName>
+            <Date>{formatDateToNow(postedAt)}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
 };
